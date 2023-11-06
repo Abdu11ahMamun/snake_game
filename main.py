09cbc2c13f78635304df3b8a590000ac19fb982d
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2023-11-07 00:22:30 +0600

    architecture ready

--- main.py
+++ main.py
@@ -1,2 +1,49 @@
+#Snake Game
+
+
+class cube(object):
+    rows=0
+    w=0
+    def __init__(self, start, dirx=1, diry=0, color=(255,0,0)):
+        pass
+
+    def move(self, dirx, diry):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+class snake(object):
+    def __init__(self,color, pos):
+        pass
+    def move(self):
+        pass
+    def reset(self, pos):
+        pass
+    def addCube(self):
+        pass
+    def draw(self, surface):
+        pass
+
+def drawGrid(w, rows, surfaces):
+    pass
+def redrawWindow(surface):
+    pass
+
+def randomSnack(rows, items):
+    pass
+
+def message_box(subject, content):
+    pass
+
+
 def main():
-    return None
+    pass
+
+rows= 0
+w= 0
+h= 0
+cube.rows =rows
+cube.w= w
+
+main()
